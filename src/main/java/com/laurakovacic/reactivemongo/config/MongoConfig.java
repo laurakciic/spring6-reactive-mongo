e7811161e6b6e32d7eedd8b72d73bdf9e7eee6d6
Laura <laurakovacic@outlook.com>
2023-04-20 13:27:22 +0200

    add mongo credentials/authentication (optional)

--- src/main/java/com/laurakovacic/reactivemongo/config/MongoConfig.java
+++ src/main/java/com/laurakovacic/reactivemongo/config/MongoConfig.java
@@ -1,10 +1,15 @@
 package com.laurakovacic.reactivemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -18,4 +23,16 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     protected String getDatabaseName() {
         return "brewery";
     }
+
+    // needed depending upon MongoDB config and where is it running from (needed for Docker)
+//    @Override
+//    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+//        builder.credential(MongoCredential.createCredential("root",
+//                        "admin", "example".toCharArray()))
+//                .applyToClusterSettings(settings -> {  // setting up the conn string for localhost
+//                    settings.hosts((singletonList(
+//                            new ServerAddress("127.0.0.1", 27017)
+//                    )));
+//                });
+//    }
 }
